ae68e50be98627484673de7dae8cc89dc8e247c6
LazyAsek <jwlochwa@gmail.com>
2022-04-27 23:51:30 +0200

    koks

--- game.js
+++ game.js
@@ -7,6 +7,15 @@ let player1Currentpostion = player1Start
 const player2Start = [560,120]
 let player2Currentpostion = player2Start
 
+const ballStart = [295,145]
+let ballCurrentposition = ballStart
+
+let xDirection = -2
+let yDirection = 2
+
+let score1 =0
+let score2 =0
+
 //draw line
 const line =document.createElement("div")
 line.classList.add("line")
@@ -28,13 +37,13 @@ function drawUser1(){
 function moveUser1(e){
     switch(e.key){
         case "w":
-            if(player1Currentpostion[1]<250){
+            if(player1Currentpostion[1]>0){
             player1Currentpostion[1] -= 10
             drawUser1()
             }
             break
         case "s":
-             if(player1Currentpostion[1]>0){
+             if(player1Currentpostion[1]<250){
                 player1Currentpostion[1]+=10
               drawUser1()
              }
@@ -72,8 +81,112 @@ function moveUser2(e){
     }
 }
 
+setInterval(moveUser1,600)
+setInterval(moveUser1,600)
 document.addEventListener("keydown",moveUser2)
 document.addEventListener("keydown",moveUser1)
+
+//add ball
+const ball = document.createElement("div")
+ball.classList.add("ball")
+drawBall()
+grid.appendChild(ball)
+
+//draw ball
+function drawBall(){
+    ball.style.left = ballCurrentposition[0]+"px"
+    ball.style.top = ballCurrentposition[1]+"px"
+}
+
+//move ball
+function moveBall(){
+ballCurrentposition[0] += xDirection
+ballCurrentposition[1] += yDirection
+checkforColisions()
+drawBall()
+setScores()
+}
+function start(){
+timerid=setInterval(moveBall,15)
+}
+//check for colisions
+
+function checkforColisions(){
+    //wall colisions
+  if( ballCurrentposition[1]<0){
+      yDirection = 2
+  }
+  if( ballCurrentposition[1]>300){
+    yDirection = -2
+}
+
+//user1 colisions
+if(
+(ballCurrentposition[0] >= player1Currentpostion[0]-10) &&
+(ballCurrentposition[0] <= player1Currentpostion[0]) &&
+(ballCurrentposition[1] >= player1Currentpostion[1]) &&
+(ballCurrentposition[1]<= player1Currentpostion[1] +50)
+){
+    xDirection = 2
+}
+
+//user 2 colisions
+if(
+    (ballCurrentposition[0] >= player2Currentpostion[0]-10) &&
+    (ballCurrentposition[0] <= player2Currentpostion[0]) &&
+    (ballCurrentposition[1] >= player2Currentpostion[1]) &&
+    (ballCurrentposition[1]<= player2Currentpostion[1] +50)
+    ){
+        xDirection = -2
+    }
+
+//wall colisions + score
+if(ballCurrentposition[0]<=0){
+    score2 +=1
+    ballCurrentposition[0] += 300
+    xDirection = 2
+    console.log(score2)
+}
+if(ballCurrentposition[0]>=590){
+    score1 +=1
+    ballCurrentposition[0] -= 300
+    xDirection = -2
+    console.log(score1)
+}
+
+
+
+//check for win
+if(score1>=10 || score2 >=10){
+
+}
+
+}
+
+//set scores
+function setScores(){
+document.getElementById("score1").innerHTML = "User 1 Score:"+score1
+document.getElementById("score2").innerHTML = "User 2 Score:"+score2
+
+//check for win user 1
+if(score1>=10){
+    alert("user 1 wins")
+    clearInterval(timerid)
+    removeEventListener("keydown",moveUser2)
+    removeEventListener("keydown",moveUser1)
+    score1 = 0
+}
+//check for win user 2
+if(score2>=10){
+    alert("user 2 wins")
+    clearInterval(timerid)
+    removeEventListener("keydown",moveUser2)
+    removeEventListener("keydown",moveUser1)
+    score2 = 0
+}
+
+}
+
 
 
 
